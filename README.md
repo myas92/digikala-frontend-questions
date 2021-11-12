210649b9c0de91187554b4566911cdf5d059a0df
yaser ahmadi <m.y.ahmadi22@gmail.com>
2021-11-12 16:22:51 +0330

    init commit, first question

---
+++ README.md
@@ -0,0 +1,44 @@
+## اسلایدر محصولات
+
+###  ساختار فایل‌های این پروژه به صورت زیر است
+```
+product-slider
+├── assets
+│   ├── css
+│   │   ├── index.css
+│   │   ├── productCard.css
+│   │   └── productSlider.css
+│   ├── font
+│   │   └── Vazir.ttf
+│   └── images
+│       ├── 01.jpg
+│       ├── 02.jpg
+│       ├── 03.jpg
+│       ├── 04.jpg
+│       ├── 05.jpg
+│       ├── 06.jpg
+│       ├── favicon.ico
+│       ├── next.png
+│       └── previous.png
+├── cypress
+│   ├── fixtures
+│   │   └── example.json
+│   ├── plugins
+│   │   └── index.js
+│   └── support
+│       ├── commands.js
+│       └── index.js
+├── data
+│   └── index.js
+├── utils
+│   └── number.js
+├── cypress.json
+├── index.html
+├── package.json
+└── script.js
+```
+
+### جزئیات
+
+ظاهر کلی برنامه به‌این صورت است:
+
---
+++ product-slider/script.js
@@ -0,0 +1,58 @@
+// Elements
+const nextBtn = document.querySelector(".js-next-btn");
+const productCard = document.querySelector(".js-product-card");
+const previousBtn = document.querySelector(".js-previous-btn");
+const productCardLink = document.querySelector(".js-product-link");
+const productCardImage = document.querySelector(".js-product-card-image");
+const productCardTitle = document.querySelector(".js-product-card-title");
+const productCardPrice = document.querySelector(".js-product-card-price");
+const productCardDiscountOldPrice = document.querySelector(
+  ".js-product-card-discount-old-price"
+);
+const productCardDiscountContainer = document.querySelector(
+  ".js-product-card-discount-container"
+);
+const productCardDiscountPercentage = document.querySelector(
+  ".js-product-card-discount-percentage"
+);
+
+const PRODUCTS = window.productsListData || [];
+
+function showProduct(product) {
+  if (!product) return;
+  productCardTitle.innerHTML = product.title;
+  productCardLink.setAttribute("href", product.url);
+  productCardImage.setAttribute("src", product.image);
+  productCard.setAttribute("data-product-id", product.id);
+  productCardPrice.innerHTML = window.formatPrice(product.price.selling_price);
+  if (product.price.discount_percent > 0) {
+    productCardDiscountContainer.classList.remove("hidden");
+    productCardDiscountOldPrice.innerHTML = window.formatPrice(
+      product.price.main_price
+    );
+    productCardDiscountPercentage.innerHTML = `${window.formatPrice(
+      product.price.discount_percent
+    )}٪`;
+  } else {
+    productCardDiscountContainer.classList.add("hidden");
+  }
+  init();
+}
+
+function init() {
+  // TODO: Complete this function
+}
+
+function nextProduct() {
+  // TODO: Complete this function
+}
+
+function previousProduct() {
+  // TODO: Complete this function
+}
+
+// Start
+showProduct(PRODUCTS[PRODUCTS.length - 1]);
+// Navigation
+nextBtn.addEventListener("click", () => nextProduct());
+previousBtn.addEventListener("click", () => previousProduct());
